1504bc008c4bcccc01b79d92db5be1360fb19d00
z.najerar <z.najerar@zairas-mbp.dhcp.wustl.edu>
2023-04-06 12:46:37 -0500

    ya

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -6,5 +6,26 @@ public class Date {
     public static void main(String[] args) {
 
     }
+    public interface List<Double> {
+    	
+    }
+    public int hashCode() {
+    	   return 0;
+    	}
+    LinkedList<Date> list = new LinkedList<Date>();
+    
+    System.out.println(list);
+    Date d1 = new Date(...stuff your constructor needs);
+    Date d2 = new Date(...same info as above, so these will equal each other);
+    list.add(d1);
+    list.add(d2);
+    list.add(d1);
+    System.out.println(list);
+    
+    HashSet<Date> set = new HashSet<Date>();
+    set.add(d1);
+    set.add(d2);
+    set.add(d1);
+    System.out.println(set);
 
 }
